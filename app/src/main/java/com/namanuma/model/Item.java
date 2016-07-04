b74d368bcddf569fbec8e0830c51b85bc3ae042f
生沼 一輝 <koinuma442993@gmail.com>
2016-07-05 00:41:53 +0900

    init

---
+++ app/src/main/java/com/namanuma/model/Item.java
@@ -0,0 +1,16 @@
+package com.namanuma.model;
+
+/**
+ * Created by oinumakazuki on 2016/07/05.
+ */
+public class Item {
+    private String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
